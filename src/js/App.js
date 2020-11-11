bd0860eac33a8888d14707167f210b670a6ec754
Nataliia Frank <nataliiafrank@gmail.com>
2020-11-11 11:17:52 -0800

    add sass for styling

--- src/js/App.js
+++ src/js/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Vocabulary from "./Vocabulary";
+import styles from './app.scss';
 
 const App = () => {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <h1>Hello</h1>
       <Vocabulary />
     </div>
--- src/js/Form.js
+++ src/js/Form.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import styles from './form.css';
+import styles from './form.scss';
 
 const Form = () => {
   return (
